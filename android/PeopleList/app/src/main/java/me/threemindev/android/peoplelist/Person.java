268d87d771924e28b870ba977dcf55e9d9e0aaf6
gimun lee <gimunlee@kaist.ac.kr>
2017-04-04 18:05:23 +0900

    android people list

---
+++ android/PeopleList/app/src/main/java/me/threemindev/android/peoplelist/Person.java
@@ -0,0 +1,17 @@
+package me.threemindev.android.peoplelist;
+
+/**
+ * Created by newtong on 2017-04-04.
+ */
+
+public class Person {
+    public String name;
+    public int age;
+
+    public Person() {
+        name = "테스트";
+    }
+    public Person(String name) {
+        this.name = name;
+    }
+}
